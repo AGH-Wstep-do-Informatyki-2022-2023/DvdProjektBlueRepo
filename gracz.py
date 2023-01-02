d78cad5da8a50760bb6cf7fa0f4cbc02d990c222
Michał Dziarkowski <115870394+mdziaro@users.noreply.github.com>
2023-01-02 20:45:00 +0100

    Update gracz.py
    poprawiłem błąd dt. koloru gracza

--- gracz.py
+++ gracz.py
@@ -3,7 +3,7 @@ class Player(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.Surface((50, 40))
-        self.image.fill(0, 255, 0)
+        self.image.fill(GREEN)
         self.rect = self.image.get_rect()
         self.rect.centerx = WIDTH / 2
         self.rect.bottom = HEIGHT - 10
