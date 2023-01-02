79713d085d80e54cbd8da3b6a27d8019351accbe
Pawel Czernecki <czerneckip1@gmail.com>
2023-01-02 21:28:59 +0100

    main uzupełniony

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import pygame
 import random
+from bullet import Bullet
+from menu import GameMenu
+from gracz import Player
+from enemy import Enemy
  
 # Inicjalizacja Pygame
 pygame.init()
@@ -13,6 +17,56 @@ FPS = 60
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Penguin Invaders")
 
+# Grupy
+all_sprites = pygame.sprite.Group()
+enemies = pygame.sprite.Group()
+bullets = pygame.sprite.Group()
+
+# Tworzenie gracza
+player = Player()
+all_sprites.add(player)
+
+# Tworzenie wrogów
+for i in range(8):
+    enemy = Enemy()
+    enemies.add(enemy)
+    all_sprites.add(enemy)
+
+# Pętla główna gry
+menu = GameMenu(screen)
+menu.run()
+running = True
+while running:
+    # Sprawdzanie wydarzeń
+    for event in pygame.event.get():       
+        if event.type == pygame.QUIT:
+            running = False
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                bullet = Bullet(player.rect.centerx, player.rect.top)
+                all_sprites.add(bullet)
+                bullets.add(bullet)
+
+    # Aktualizacja
+    all_sprites.update()
+
+    # Sprawdzenie, czy pocisk trafił wroga
+    hits = pygame.sprite.groupcollide(enemies, bullets, True, True)
+    for hit in hits:
+        enemy = Enemy()
+        all_sprites.add(enemy)
+        enemies.add(enemy)
+
+    # Sprawdzenie, czy gracz trafił wroga
+    hits = pygame.sprite.spritecollide(player, enemies, False)
+    if hits:
+        running = False
+
+    # Rysowanie ekranu
+    screen.fill(BLACK)
+    all_sprites.draw(screen)
+    pygame.display.flip()
+    clock.tick(FPS)
 # Zegar
 clock = pygame.time.Clock()
 pygame.quit()
