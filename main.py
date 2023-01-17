c335deba94e68d51dd291a26ce0a46b077a667ec
jkb <Jacob202@protonmail.com>
2023-01-17 18:39:51 +0100

    demo2

--- main.py
+++ main.py
@@ -5,10 +5,7 @@ from menu import GameMenu
 from gracz import Player
 from enemy import Enemy
 from global_settings import Window, Colors
- 
-# Inicjalizacja Pygame
-pygame.init()
- 
+
 # Ustawienia gry
 WIDTH = Window.WIDTH
 HEIGHT = Window.HEIGHT
@@ -20,36 +17,40 @@ RED = Colors.RED
 GREEN = Colors.GREEN
 BLUE = Colors.BLUE
 
-clock = pygame.time.Clock()
-clock.tick(FPS)
-
-# Stworzenie okna gry
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("Penguin Invaders")
-
-# Grupy
-all_sprites = pygame.sprite.Group()
-enemies = pygame.sprite.Group()
-bullets = pygame.sprite.Group()
-
-# Tworzenie gracza
-player = Player()
-all_sprites.add(player)
-
-# Tworzenie wrogów
-for i in range(8):
-    enemy = Enemy()
-    enemies.add(enemy)
-    all_sprites.add(enemy)
-
 while True:
+    # Inicjalizacja Pygame
+    pygame.init()
+
+    clock = pygame.time.Clock()
+    clock.tick(FPS)
+
+    # Stworzenie okna gry
+    screen = pygame.display.set_mode((WIDTH, HEIGHT))
+    pygame.display.set_caption("Penguin Invaders")
+
+    # Grupy
+    all_sprites = pygame.sprite.Group()
+    enemies = pygame.sprite.Group()
+    bullets = pygame.sprite.Group()
+
+    # Tworzenie gracza
+    player = Player()
+    all_sprites.add(player)
+
+    # Tworzenie wrogów
+    for i in range(8):
+        enemy = Enemy()
+        enemies.add(enemy)
+        all_sprites.add(enemy)
+
+
     # Pętla główna gry
     menu = GameMenu(screen)
     menu.run()
     running = True
     while running:
         # Sprawdzanie wydarzeń
-        for event in pygame.event.get():       
+        for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
             elif event.type == pygame.KEYDOWN:
@@ -77,5 +78,5 @@ while True:
         screen.fill(BLACK)
         all_sprites.draw(screen)
         pygame.display.flip()
-        
-pygame.quit()
+
+    pygame.quit()
